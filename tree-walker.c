a8309e15b3b21bde2984cab71ef1085694b176fd
Roman Senn <roman.l.senn@gmail.com>
2021-02-13 02:06:33 +0100

    ...

--- tree-walker.c
+++ tree-walker.c
@@ -68,17 +68,17 @@ tree_iterator_init(TreeIterator* it, JSContext* ctx, JSValueConst object, int fl
   return 0;
 }
 
-static int32_t
+static int
 tree_iterator_setpos(TreeIterator* it, int32_t idx) {
-  if(idx < 0) {
-    idx %= (int32_t)it->tab_atom_len;
+  if((idx < 0 ? -idx : idx) >= it->tab_atom_len)
+    return 0;
+
+  if(idx < 0)
     idx += it->tab_atom_len;
-  } else {
-    idx = (uint32_t)idx % (uint32_t)it->tab_atom_len;
-  }
   assert(idx >= 0);
   assert(idx < it->tab_atom_len);
-return (it->idx = idx);
+  it->idx = idx;
+  return 1;
 }
 
 static JSValue
@@ -94,8 +94,8 @@ tree_iterator_key(TreeIterator* it, JSContext* ctx) {
 static TreeIterator*
 tree_walker_setroot(TreeWalker* wc, JSContext* ctx, JSValueConst object) {
   TreeIterator* it;
-
-  if((it = tree_iterator_new(wc, JS_DupValue(ctx, object))))
+  wc->current = JS_DupValue(ctx, object);
+  if((it = tree_iterator_new(wc, wc->current)))
     tree_iterator_init(it, ctx, object, JS_GPN_STRING_MASK | JS_GPN_SYMBOL_MASK | JS_GPN_ENUM_ONLY);
   return it;
 }
@@ -108,18 +108,6 @@ tree_walker_push(TreeWalker* wc, JSContext* ctx) {
     tree_iterator_init(it, ctx, wc->current, JS_GPN_STRING_MASK | JS_GPN_SYMBOL_MASK | JS_GPN_ENUM_ONLY);
   return it;
 }
-
-/*static JSValue
-tree_iterator_propdesc(TreeIterator* it, int32_t idx, JSContext* ctx) {
-  JSPropertyEnum* prop;
-  if((prop = tree_iterator_setpos(it, idx))) {
-    JSPropertyDescriptor desc;
-
-    JS_GetOwnProperty(ctx, &desc, it->obj, prop->atom);
-  }
-
-  return JS_UNDEFINED;
-}*/
 
 static JSValue
 js_tree_walker_ctor(JSContext* ctx, JSValueConst new_target, int argc, JSValueConst* argv) {
@@ -158,30 +146,30 @@ js_tree_walker_method(JSContext* ctx, JSValueConst this_val, int argc, JSValueCo
   TreeWalker* wc;
   TreeIterator* it;
 
-  if(!(wc = JS_GetOpaque2(ctx, /*ctx, */ this_val, js_tree_walker_class_id)))
+  if(!(wc = JS_GetOpaque2(ctx, this_val, js_tree_walker_class_id)))
     return JS_EXCEPTION;
   it = vector_back(&wc->frames, sizeof(TreeIterator));
 
+  /*  if(!JS_IsObject(wc->current))
+      return JS_EXCEPTION;*/
+
   switch(magic) {
     case FIRST_CHILD: {
-      if(JS_IsObject(wc->current)) {
-        tree_iterator_setpos((it = tree_walker_push(wc, ctx)), 0);
+      if(tree_iterator_setpos((it = tree_walker_push(wc, ctx)), 0))
         return wc->current = tree_iterator_value(it, ctx);
-      }
       break;
     }
     case LAST_CHILD: {
-      if(JS_IsObject(wc->current)) {
-        ;
-        tree_iterator_setpos((it = tree_walker_push(wc, ctx)), -1);
+      if(tree_iterator_setpos((it = tree_walker_push(wc, ctx)), -1))
         return wc->current = tree_iterator_value(it, ctx);
-      }
       break;
     }
     case NEXT_NODE: {
       break;
     }
     case NEXT_SIBLING: {
+      if(tree_iterator_setpos(it, it->idx + 1))
+        return wc->current = tree_iterator_value(it, ctx);
       break;
     }
     case PARENT_NODE: {
@@ -191,6 +179,8 @@ js_tree_walker_method(JSContext* ctx, JSValueConst this_val, int argc, JSValueCo
       break;
     }
     case PREVIOUS_SIBLING: {
+      if(tree_iterator_setpos(it, it->idx - 1))
+        return wc->current = tree_iterator_value(it, ctx);
       break;
     }
   }
@@ -264,18 +254,19 @@ JSClassDef js_tree_walker_class = {
 };
 
 static const JSCFunctionListEntry js_tree_walker_proto_funcs[] = {
-    JS_CFUNC_MAGIC_DEF("firstChild", 1, js_tree_walker_method, FIRST_CHILD),
-    JS_CFUNC_MAGIC_DEF("lastChild", 1, js_tree_walker_method, LAST_CHILD),
-    JS_CFUNC_MAGIC_DEF("nextNode", 1, js_tree_walker_method, NEXT_NODE),
-    JS_CFUNC_MAGIC_DEF("nextSibling", 1, js_tree_walker_method, NEXT_SIBLING),
-    JS_CFUNC_MAGIC_DEF("parentNode", 1, js_tree_walker_method, PARENT_NODE),
-    JS_CFUNC_MAGIC_DEF("previousNode", 1, js_tree_walker_method, PREVIOUS_NODE),
-    JS_CFUNC_MAGIC_DEF("previousSibling", 1, js_tree_walker_method, PREVIOUS_SIBLING),
+    JS_CFUNC_MAGIC_DEF("firstChild", 0, js_tree_walker_method, FIRST_CHILD),
+    JS_CFUNC_MAGIC_DEF("lastChild", 0, js_tree_walker_method, LAST_CHILD),
+    JS_CFUNC_MAGIC_DEF("nextNode", 0, js_tree_walker_method, NEXT_NODE),
+    JS_CFUNC_MAGIC_DEF("nextSibling", 0, js_tree_walker_method, NEXT_SIBLING),
+    JS_CFUNC_MAGIC_DEF("parentNode", 0, js_tree_walker_method, PARENT_NODE),
+    JS_CFUNC_MAGIC_DEF("previousNode", 0, js_tree_walker_method, PREVIOUS_NODE),
+    JS_CFUNC_MAGIC_DEF("previousSibling", 0, js_tree_walker_method, PREVIOUS_SIBLING),
     JS_CGETSET_MAGIC_DEF("root", js_tree_walker_get, NULL, GET_ROOT),
     JS_CGETSET_MAGIC_DEF("currentNode", js_tree_walker_get, NULL, GET_CURRENT_NODE),
     JS_CGETSET_MAGIC_DEF("depth", js_tree_walker_get, NULL, GET_DEPTH),
     JS_CGETSET_MAGIC_DEF("index", js_tree_walker_get, NULL, GET_INDEX),
     JS_CGETSET_MAGIC_DEF("length", js_tree_walker_get, NULL, GET_LENGTH),
+    JS_CGETSET_MAGIC_DEF("key", js_tree_walker_get, NULL, GET_KEY),
     JS_PROP_STRING_DEF("[Symbol.toStringTag]", "TreeWalker", JS_PROP_CONFIGURABLE)};
 
 static const JSCFunctionListEntry js_tree_walker_static_funcs[] = {
