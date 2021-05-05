b0a77819367769e8920417b9f22d2fa5d644189f
Roman Senn <roman.l.senn@gmail.com>
2021-05-05 09:47:08 +0200

    ...

--- tests/test_console.js
+++ tests/test_console.js
@@ -4,18 +4,16 @@ import inspect from 'inspect';
 import * as deep from 'deep';
 import Console from 'console';
 
-const inspectOptions = {
-  colors: true,
-  showHidden: false,
-  showProxy: false,
-  stringBreakNewline: true,
-  maxStringLength: Infinity,
-  compact: 1,
-  numberBase: 16
-};
-
 function main(...args) {
-  console = new Console(inspectOptions);
+  console = new Console({
+    colors: true,
+    showHidden: false,
+    showProxy: false,
+    stringBreakNewline: true,
+    maxStringLength: Infinity,
+    compact: 1,
+    numberBase: 16
+  });
 
   const testString =
     '/*\n * [The "BSD license"]\n *  Copyright (c) 2012-2015 Terence Parr\n *  Copyright (c) 2012-2015 Sam Harwell\n *  Copyright (c) 2015 Gerald Rosenberg\n *  All rights reserved.\n *\n *  Redistribution and use in source and binary forms, with or without\n *  modification, are permitted provided that the following conditions\n *  are met:\n *\n *  1. Redistributions of source code must retain the above copyright\n *     notice, this list of conditions and the following disclaimer.\n *  2. Redistributions in binary form must reproduce the above copyright\n *     notice, this list of conditions and the following disclaimer in the\n *     documentation and/or other materials provided with the distribution.\n *  3. The name of the author may not be used to endorse or promote products\n *     derived from this software without specific prior written permission.\n *\n *  THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS\'\' AND ANY EXPRESS OR\n *  IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES\n *  OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.\n *  IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,\n *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT\n *  NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,\n *  DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY\n *  THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT\n *  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF\n *  THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.\n */\n/**\n *\tA grammar for ANTLR v4 implemented using v4 syntax\n *\n *\tModified 2015.06.16 gbr\n *\t-- update for compatibility with Antlr v4.5\n */\n\n// ======================================================\n// Lexer specification\n// ======================================================\n\nlexer grammar ANTLRv4Lexer;\n\noptions { superClass = LexerAdaptor; }\nimport LexBasic;\n\n// Standard set of fragments\ntokens { TOKEN_REF , RULE_REF , LEXER_CHAR_SET }\nchannels { OFF_CHANNEL , COMMENT }\n\n// -------------------------\n// Comments\nDOC_COMMENT\n   : DocComment -> channel (COMMENT)\n   ;\n\nBLOCK_COMMENT\n   : BlockComment -> channel (COMMENT)\n   ;\n\nLINE_COMMENT\n   : LineComment -> channel (COMMENT)\n   ;\n\n// -------------------------\n// Integer\n\nINT\n   : DecimalNumeral\n   ;\n\n// -------------------------\n// Literal string\n//\n// ANTLR makes no distinction between a single character literal and a\n// multi-character string. All literals are single quote delimited and\n';
--- tests/test_inspect.js
+++ tests/test_inspect.js
@@ -49,7 +49,9 @@ async function main(...args) {
     compact: Infinity,
     hideKeys: ['loc', 'range', 'inspect', Symbol.for('nodejs.util.inspect.custom')]
   };
-
+  let arr = new Uint8Array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+  console.log('arr', arr);
+return;
   console.log('inspect(Test.prototype)', inspect(Test.prototype, options));
   const dumpObj = (obj, depth, options) =>
     '{' +
@@ -61,7 +63,6 @@ async function main(...args) {
   let value = 0;
   let str =
     '2.6 Test262 (ECMAScript Test Suite)\nA test262 runner is included in the QuickJS archive. The test262 tests can be installed in the\nQuickJS source directory with:\ngit clone https://github.com/tc39/test262.git test262\ncd test262\npatch -p1 < ../tests/test262.patch\ncd ..\nThe patch adds the implementation specific harness functions and optimizes the inefficient\nRegExp character classes and Unicode property escapes tests (the tests themselves are not\nmodified, only a slow string initialization function is optimized).\nThe tests can be run with\nmake test2\nThe configuration files test262.conf (resp. test262o.conf for the old ES5.1 tests1 )) contain\nthe options to run the various tests. Tests can be excluded based on features or filename.\nThe file test262_errors.txt contains the current list of errors. The runner displays a\nmessage when a new error appears or when an existing error is corrected or modified. Use the\n-u option to update the current list of errors (or make test2-update).\nThe file test262_report.txt contains the logs of all the tests. It is useful to have a clearer\nanalysis of a particular error. In case of crash, the last line corresponds to the failing test.\nUse the syntax ./run-test262 -c test262.conf -f filename.js to run a single test. Use\nthe syntax ./run-test262 -c test262.conf N to start testing at test number N.\nFor more information, run ./run-test262 to see the command line options of the test262\nrunner.\nrun-test262 accepts the -N option to be invoked from test262-harness2 thru eshost.\nUnless you want to compare QuickJS with other engines under the same conditions, we do not\nrecommend to run the tests this way as it is much slower (typically half an hour instead of about\n100 seconds).\n';
-  let arr = new Uint32Array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
   let obj2 = {
     a: 1,
     b: true,
