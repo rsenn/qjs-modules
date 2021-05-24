aacb15201dde346d605c4711ac4e25da555e7f57
Roman Senn <roman.l.senn@gmail.com>
2021-05-24 08:22:51 +0200

    ...

--- lexer.c
+++ lexer.c
@@ -92,7 +92,8 @@ lexer_rule_compile(Lexer* lex, LexerRule* rule, JSContext* ctx) {
 
   if(lexer_rule_expand(lex, p, &dbuf)) {
     rule->expansion = js_strndup(ctx, (const char*)dbuf.buf, dbuf.size);
-    rule->bytecode = regexp_compile(regexp_from_dbuf(&dbuf, LRE_FLAG_GLOBAL | LRE_FLAG_STICKY), ctx);
+    rule->bytecode =
+        regexp_compile(regexp_from_dbuf(&dbuf, LRE_FLAG_GLOBAL | LRE_FLAG_MULTILINE | LRE_FLAG_STICKY), ctx);
     ret = rule->bytecode != 0;
 
   } else {
