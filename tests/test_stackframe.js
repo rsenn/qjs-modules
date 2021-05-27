a5370e49e5194bf6fef8117aa561ac547a596f74
Roman Senn <roman.l.senn@gmail.com>
2021-05-27 08:31:26 +0200

    ...

---
+++ tests/test_stackframe.js
@@ -0,0 +1,52 @@
+import * as os from 'os';
+import * as std from 'std';
+import inspect from 'inspect';
+import { Console } from 'console';
+import {Stack,StackFrame} from '../lib/stackframe.js';
+
+function Func1() {
+  return Func2();
+}
+function Func2() {
+  return Func3();
+}
+function Func3() {
+  return Func4();
+}
+
+function Func4() {
+  let err = new Error();
+
+  return new Stack (err.stack );
+}
+
+function main(...args) {
+  globalThis.console = new Console({
+    inspectOptions: {
+      colors: true,
+      depth: 8,
+      breakLength: 100,
+      maxStringLength: Infinity,
+      maxArrayLength: Infinity,
+      compact: 0,
+      showHidden: false
+    }
+  });
+  let stack = Func1();
+  console.log('stack', stack);
+let frame = stack[0];
+
+  console.log('frame', frame);
+  console.log('frame.toString()', frame.toString());
+
+  std.gc();
+}
+
+try {
+  main(...scriptArgs.slice(1));
+} catch(error) {
+  console.log(`FAIL: ${error.message}\n${error.stack}`);
+  std.exit(1);
+} finally {
+  console.log('SUCCESS');
+}
