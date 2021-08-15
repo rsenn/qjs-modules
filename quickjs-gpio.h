ac8e716dcc297aeaf01528653f9fd131c2cce1fd
Roman Senn <roman.l.senn@gmail.com>
2021-08-15 02:24:50 +0200

    ...

--- quickjs-gpio.h
+++ quickjs-gpio.h
@@ -6,8 +6,6 @@ struct gpio;
 #include <quickjs.h>
 #include <cutils.h>
 
-extern thread_local JSClassID js_gpio_class_id;
-
 struct gpio* js_gpio_data(JSContext*, JSValue value);
 int js_gpio_init(JSContext*, JSModuleDef* m);
 JSModuleDef* js_init_module_gpio(JSContext*, const char* module_name);
