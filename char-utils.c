651160c57004312d88fbc5fd855e243cd4f2147f
Roman Senn <roman.l.senn@gmail.com>
2021-09-03 22:36:00 +0200

    ...

---
+++ char-utils.c
@@ -0,0 +1,18 @@
+#include "char-utils.h"
+
+size_t
+token_length(const char* str, size_t len, char delim) {
+  const char *s, *e;
+  size_t pos;
+  for(s = str, e = s + len; s < e; s += pos + 1) {
+    pos = byte_chr(s, e - s, delim);
+    if(s + pos == e)
+      break;
+
+    if(pos == 0 || s[pos - 1] != '\\') {
+      s += pos;
+      break;
+    }
+  }
+  return s - str;
+}
