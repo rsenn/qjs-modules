cf29faf79233607aaae8da2ecede1efaebd7741f
Roman Senn <roman.l.senn@gmail.com>
2021-12-10 18:42:43 +0100

    ...

--- utils.c
+++ utils.c
@@ -2229,7 +2229,7 @@ js_error_tostring(JSContext* ctx, JSValueConst error) {
 
 void
 js_error_print(JSContext* ctx, JSValueConst error) {
-  const char *str=0, *stack = 0;
+  const char *str = 0, *stack = 0;
 
   if(JS_IsObject(error)) {
     JSValue st = JS_GetPropertyStr(ctx, error, "stack");
