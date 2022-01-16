d9d0b5686a3066bf266cd913146db5479593045e
Roman Senn <roman.l.senn@gmail.com>
2022-01-16 08:45:59 +0100

    ...

--- tests/test_dom.js
+++ tests/test_dom.js
@@ -1,12 +1,12 @@
 import * as os from 'os';
 import * as std from 'std';
-import { escape, quote, isObject, define, getClassName } from '../lib/util.js';
+import { escape, quote, isObject, define, getClassName, mapObject, getset, gettersetter, memoize } from '../lib/util.js';
 import inspect from 'inspect';
 import * as xml from 'xml';
 import * as path from 'path';
 import * as deep from 'deep';
 import Console from '../lib/console.js';
-import { Document, Element, Node, Attr } from '../lib/dom.js';
+import { Document, Element, Node, Attr, Factory, NamedNodeMap } from '../lib/dom.js';
 
 function main(...args) {
   globalThis.console = new Console(process.stdout, {
@@ -34,6 +34,19 @@ function main(...args) {
 
   let doc = new Document(result[0]);
 
+  let rawDoc = Node.raw(doc);
+
+  let gs = gettersetter(new Map());
+  console.log('gs', gs);
+  let mo = mapObject(gs);
+  console.log('mo', mo);
+  gs = gettersetter(mo);
+  console.log('gs', gs);
+  console.log('getset', getset(gs));
+  console.log('getset', getset(mo));
+
+  memoize((node, ...args) => new Node(node, parent));
+
   console.log('doc', inspect(doc, { depth: 20, compact: false }));
   console.log('doc.children', doc.children);
   console.log('doc.attributes', doc.attributes);
@@ -50,7 +63,7 @@ function main(...args) {
   Recurse(doc, (node, stack) => {
     const raw = Node.raw(node);
 
-    console.log('node.path', Node.path(node));
+    //console.log('node.path', Node.path(node));
     //console.log('node.ownerDocument', node.ownerDocument);
     if(node.nodeType != node.ELEMENT_NODE && node.nodeType != node.DOCUMENT_NODE) {
       /*console.log('node.nodeType', Node.types[node.nodeType]);
@@ -58,6 +71,35 @@ function main(...args) {
       console.log('node.path', Node.path(node));*/
       return;
     }
+
+    //console.log('raw.children', raw.children);
+    if(raw.children) {
+      let cl = Factory(raw.children, node);
+      console.log('cl', cl);
+
+      if(raw.children[0]) {
+        //console.log('raw.children[0]', raw.children[0]);
+        console.log('cl.path', cl.path);
+        let elm = Factory(raw.children[0], cl, Element);
+        console.log('elm', elm);
+        console.log('elm.path', elm.path);
+ console.log('elm.ownerDocument', elm.ownerDocument);
+      }
+    }
+
+    if(raw.attributes) {
+      //console.log('raw.attributes', raw.attributes);
+      let al = Factory(raw.attributes, node, NamedNodeMap);
+      console.log('al', al, al.path);
+
+      let at = al[Object.keys(raw.attributes)[0]];
+
+      if(at) {
+        console.log('at', at,at.path);
+        console.log('at.ownerElement', at.ownerElement, at.ownerElement.path);
+      }
+    }
+
     console.log('node.nextSibling', node.nextSibling, Node.types[node.nodeType], Node.path(node).slice(-2));
 
     /*else {
