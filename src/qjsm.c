3efd72dd71f3db15425d23abe191c8badfa352a6
Roman Senn <roman.l.senn@gmail.com>
2022-03-23 07:42:23 +0100

    ...

--- src/qjsm.c
+++ src/qjsm.c
@@ -1430,10 +1430,22 @@ main(int argc, char** argv) {
 #endif
     js_std_add_helpers(ctx, argc - optind, argv + optind);
 
-    {
-      const char* str = "import process from 'process';\nglobalThis.process = process;\n";
-      js_eval_str(ctx, str, "<process>", JS_EVAL_TYPE_MODULE);
-    }
+    DynBuf init_script;
+
+    js_dbuf_init(ctx, &init_script);
+
+    dbuf_putstr(&init_script,
+                "import process from 'process';\n"
+                "globalThis.process = process;\n");
+
+    dbuf_putstr(&init_script,
+                "import { require } from 'require';\n"
+                "globalThis.require = require;\n");
+
+    /*    {
+          const char* str = "import process from 'process';\nglobalThis.process = process;\n";
+          js_eval_str(ctx, str, "<process>", JS_EVAL_TYPE_MODULE);
+        }*/
     {
       JSModuleDef* req;
 
@@ -1456,12 +1468,18 @@ main(int argc, char** argv) {
     JS_SetPropertyFunctionList(ctx, JS_GetGlobalObject(ctx), jsm_global_funcs, countof(jsm_global_funcs));
     if(load_std) {
       const char* str = "import * as std from 'std';\n"
-                        "import * as os from 'os';\nglobalThis.std = std;\n"
+                        "import * as os from 'os';\n"
+                        "globalThis.std = std;\n"
                         "globalThis.os = os;\n"
-                        "globalThis.setTimeout = "
-                        "os.setTimeout;\n"
+                        "globalThis.setTimeout = os.setTimeout;\n"
                         "globalThis.clearTimeout = os.clearTimeout;\n";
-      js_eval_str(ctx, str, "<std-os>", JS_EVAL_TYPE_MODULE);
+      dbuf_putstr(&init_script, str);
+    }
+
+    if(init_script.size) {
+      dbuf_0(&init_script);
+
+      js_eval_str(ctx, init_script.buf, "<init>", JS_EVAL_TYPE_MODULE);
     }
 
     // jsm_list_modules(ctx);
