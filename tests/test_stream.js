497735e10c31b3e3393b9807caf4967bb3915480
Roman Senn <roman.l.senn@gmail.com>
2022-11-06 16:57:49 +0100

    ...

--- tests/test_stream.js
+++ tests/test_stream.js
@@ -3,7 +3,7 @@ import * as std from 'std';
 import { Console } from 'console';
 import { ReadableStream, WritableStream } from 'stream';
 import { toString, define } from 'util';
-import { FileSystemReadableFileStream, FileSystemWritableFileStream, FileSystemReadableStream, StreamReadIterator } from '../lib/streams.js';
+import { FileSystemReadableFileStream, LineStreamIterator, FileSystemWritableFileStream, FileSystemReadableStream, StreamReadIterator } from '../lib/streams.js';
 import fs from 'fs';
 import { extendAsyncGenerator } from '../lib/extendAsyncGenerator.js';
 
@@ -27,14 +27,25 @@ async function main(...args) {
   let read = new FileSystemReadableFileStream('tests/test1.xml');
 
   let iter = await StreamReadIterator(st);
+  /*  let tfrm=  LineStreamIterator(iter);*/
+  let result = await iter.reduce(
+    (buf => (acc, n) => {
+      buf += toString(n);
+      if(buf.indexOf('\n') != -1) {
+        let lines = buf.split('\n');
+        buf = lines.pop();
+        (acc ??= []).push(...lines);
+      }
+      return acc;
+    })('')
+  );
 
-  let tfrm = iter.map(n => toString(n));
+  /*let linestream=await LineStreamIterator(iter);
+console.log('linestream',  linestream );
+let result = await  linestream.reduce((acc, n) => (acc.push(n), acc), [])*/
 
-  let result = await tfrm.reduce((acc, n) => acc + n, '');
-
-  console.log('result', { result });
-  //console.log('result', result.split(/\n/g));
-  /*  
+  console.log('result', result);
+  /*
   let write = new FileSystemWritableFileStream('/tmp/out.txt');
   */
 }
