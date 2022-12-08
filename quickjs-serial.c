effbbf55d14cb35425bb8076678898d7fd927736
Roman Senn <roman.l.senn@gmail.com>
2022-12-08 10:37:58 +0100

    ...

--- quickjs-serial.c
+++ quickjs-serial.c
@@ -13,7 +13,11 @@ thread_local VISIBLE JSClassID js_serialport_class_id = 0;
 thread_local JSValue serialport_proto = {{JS_TAG_UNDEFINED}}, serialport_ctor = {{JS_TAG_UNDEFINED}}, serial_ctor = {{JS_TAG_UNDEFINED}};
 
 enum {
-  SERIALPORT_GETINFO = 0,
+  SERIALPORT_OPEN = 0,
+  SERIALPORT_CLOSE,
+  SERIALPORT_GETINFO,
+  SERIALPORT_GETSIGNALS,
+  SERIALPORT_SETSIGNALS,
   SERIALPORT_FD,
 };
 
@@ -39,6 +43,76 @@ js_serialport_method(JSContext* ctx, JSValueConst this_val, int argc, JSValueCon
     return JS_EXCEPTION;
 
   switch(serial) {
+    case SERIALPORT_OPEN: {
+      int32_t flags = SP_MODE_READ_WRITE, baud_rate = -1, data_bits = -1, stop_bits = -1;
+      const char *parity = 0, *flow_control = 0;
+      if(argc > 0) {
+        if(JS_IsNumber(argv[0])) {
+          JS_ToInt32(ctx, &flags, argv[0]);
+        } else if(JS_IsObject(argv[0])) {
+          JSValue br = JS_GetPropertyStr(ctx, argv[0], "baudRate"), db = JS_GetPropertyStr(ctx, argv[0], "dataBits"),
+                  sb = JS_GetPropertyStr(ctx, argv[0], "stopBits"), pr = JS_GetPropertyStr(ctx, argv[0], "parity"),
+                  fc = JS_GetPropertyStr(ctx, argv[0], "flowControl");
+
+          if(JS_IsNumber(br))
+            JS_ToInt32(ctx, &baud_rate, br);
+          if(JS_IsNumber(db))
+            JS_ToInt32(ctx, &data_bits, db);
+          if(JS_IsNumber(sb))
+            JS_ToInt32(ctx, &stop_bits, sb);
+          if(JS_IsString(pr))
+            parity = JS_ToCString(ctx, pr);
+          if(JS_IsString(fc))
+            flow_control = JS_ToCString(ctx, fc);
+
+          JS_FreeValue(ctx, br);
+          JS_FreeValue(ctx, db);
+          JS_FreeValue(ctx, sb);
+          JS_FreeValue(ctx, pr);
+          JS_FreeValue(ctx, fc);
+        }
+      }
+
+      if(sp_open(port, flags) != SP_OK) {
+        char* msg = sp_last_error_message();
+        sp_free_port(port);
+        ret = JS_ThrowInternalError(ctx, "failed opening port '%s': %s", sp_get_port_name(port), msg);
+        sp_free_error_message(msg);
+      } else {
+        if(baud_rate != -1) {
+          if(sp_set_baudrate(port, baud_rate) != SP_OK)
+            ret = JS_ThrowInternalError(ctx, "Failed setting baud rate on port '%s' to %" PRId32, sp_get_port_name(port), baud_rate);
+        }
+        if(parity) {
+          if(!strcmp(parity, "none")) {
+            sp_set_parity(port, SP_PARITY_NONE);
+          } else if(!strcmp(parity, "even")) {
+            sp_set_parity(port, SP_PARITY_EVEN);
+          } else if(!strcmp(parity, "odd")) {
+            sp_set_parity(port, SP_PARITY_ODD);
+          }
+        }
+        if(flow_control) {
+          if(!strcmp(flow_control, "none")) {
+            sp_set_flowcontrol(port, SP_FLOWCONTROL_NONE);
+          } else if(!strcmp(flow_control, "hardware")) {
+            sp_set_flowcontrol(port, SP_FLOWCONTROL_RTSCTS);
+          } else if(!strcmp(flow_control, "software")) {
+            sp_set_flowcontrol(port, SP_FLOWCONTROL_XONXOFF);
+          }
+        }
+      }
+      if(parity)
+        JS_FreeCString(ctx, parity);
+      if(flow_control)
+        JS_FreeCString(ctx, flow_control);
+
+      break;
+    }
+    case SERIALPORT_CLOSE: {
+      sp_close(port);
+      break;
+    }
     case SERIALPORT_GETINFO: {
       int vid = -1, pid = -1;
 
@@ -66,6 +140,42 @@ js_serialport_method(JSContext* ctx, JSValueConst this_val, int argc, JSValueCon
         JS_SetPropertyStr(ctx, ret, "description", JS_NewString(ctx, str));
       }
 
+      break;
+    }
+    case SERIALPORT_GETSIGNALS: {
+      enum sp_signal signals;
+
+      if(sp_get_signals(port, &signals) == SP_OK) {
+        if(JS_IsUndefined(ret))
+          ret = JS_NewObject(ctx);
+
+        JS_SetPropertyStr(ctx, ret, "clearToSend", JS_NewBool(ctx, signals & SP_SIG_CTS));
+        JS_SetPropertyStr(ctx, ret, "dataCarrierDetect", JS_NewBool(ctx, signals & SP_SIG_DCD));
+        JS_SetPropertyStr(ctx, ret, "dataSetReady", JS_NewBool(ctx, signals & SP_SIG_DSR));
+        JS_SetPropertyStr(ctx, ret, "ringIndicator", JS_NewBool(ctx, signals & SP_SIG_RI));
+      }
+
+      break;
+    }
+    case SERIALPORT_SETSIGNALS: {
+      JSValue dtr, rts, brk;
+
+      dtr = JS_GetPropertyStr(ctx, argv[0], "dataTerminalReady");
+      rts = JS_GetPropertyStr(ctx, argv[0], "requestToSend");
+      brk = JS_GetPropertyStr(ctx, argv[0], "break");
+
+      if(JS_IsBool(dtr))
+        sp_set_dtr(port, JS_ToBool(ctx, dtr) ? SP_DTR_ON : SP_DTR_OFF);
+
+      if(JS_IsBool(rts))
+        sp_set_rts(port, JS_ToBool(ctx, rts) ? SP_RTS_ON : SP_RTS_OFF);
+
+      if(JS_IsBool(brk))
+        JS_ToBool(ctx, rts) ? sp_start_break(port) : sp_end_break(port);
+
+      JS_FreeValue(ctx, dtr);
+      JS_FreeValue(ctx, rts);
+      JS_FreeValue(ctx, brk);
       break;
     }
   }
@@ -133,14 +243,8 @@ js_serial_requestport(JSContext* ctx, JSValueConst this_val, int argc, JSValueCo
 
     if(sp_get_port_by_name(port_name, &port) == SP_OK) {
 
-      if(sp_open(port, SP_MODE_READ_WRITE) != SP_OK) {
-        char* msg = sp_last_error_message();
-        sp_free_port(port);
-        ret = JS_ThrowInternalError(ctx, "failed opening port '%s': %s", port_name, msg);
-        sp_free_error_message(msg);
-      } else {
+      if(JS_IsUndefined(ret))
         ret = js_serialport_wrap(ctx, port);
-      }
     } else {
       ret = JS_ThrowInternalError(ctx, "Serial port '%s' not found", port_name);
     }
@@ -152,7 +256,11 @@ js_serial_requestport(JSContext* ctx, JSValueConst this_val, int argc, JSValueCo
 }
 
 static const JSCFunctionListEntry js_serialport_funcs[] = {
+    JS_CFUNC_MAGIC_DEF("open", 0, js_serialport_method, SERIALPORT_OPEN),
+    JS_CFUNC_MAGIC_DEF("close", 0, js_serialport_method, SERIALPORT_CLOSE),
     JS_CFUNC_MAGIC_DEF("getInfo", 0, js_serialport_method, SERIALPORT_GETINFO),
+    JS_CFUNC_MAGIC_DEF("getSignals", 0, js_serialport_method, SERIALPORT_GETSIGNALS),
+    JS_CFUNC_MAGIC_DEF("setSignals", 1, js_serialport_method, SERIALPORT_SETSIGNALS),
     JS_CGETSET_MAGIC_DEF("fd", js_serialport_get, 0, SERIALPORT_FD),
 
 };
