586114d05de41353fbbef1f3ff8f842466901778
Roman Senn <roman.l.senn@gmail.com>
2023-03-08 00:03:45 +0100

    ...

--- tests/test_lexer.js
+++ tests/test_lexer.js
@@ -15,21 +15,19 @@ import { define, curry, unique, split, extendArray, isObject, getOpt, keys } fro
 import { mmap, munmap, PROT_READ, PROT_WRITE, MAP_PRIVATE, MAP_SHARED, MAP_ANONYMOUS } from 'mmap';
 import { TryCatch } from '../lib/parser/ebnf.js';
 
-let buffers = {},
-  modules = {};
-let T;
-
 ('use strict');
 ('use math');
+
+let buffers = {},
+  modules = {};
+
+let T;
 
 extendArray(Array.prototype);
 
 const AddUnique = (arr, item) => (arr.indexOf(item) == -1 ? arr.push(item) : null);
-
 const IntToDWord = ival => (isNaN(ival) === false && ival < 0 ? ival + 4294967296 : ival);
 const IntToBinary = i => (i == -1 || typeof i != 'number' ? i : '0b' + IntToDWord(i).toString(2));
-
-//const code = ["const str = stack.toString().replace(/\\n\\s*at /g, '\\n');", "/^(.*)\\s\\((.*):([0-9]*):([0-9]*)\\)$/.exec(line);" ];
 const code = [
   "const str = stack.toString().replace(/\\n\\s*at /g, '\\n');",
   '/Reg.*Ex/i.test(n)',
@@ -37,8 +35,6 @@ const code = [
   'const [match, pattern, flags] = /^\\/(.*)\\/([a-z]*)$/.exec(token.value);',
   '/^\\s\\((.*):([0-9]*):([0-9]*)\\)$/.exec(line);'
 ];
-
-extendArray(Array.prototype);
 
 const bufferRef = new WeakMap();
 
@@ -53,10 +49,8 @@ function BufferFile(file) {
     }
     const fd = os.open(file, os.O_RDONLY);
     console.log('mmap', [0, size, PROT_READ, MAP_PRIVATE, fd, 0]);
-
     b = mmap(0, size, PROT_READ, MAP_PRIVATE, fd, 0);
   }
-
   bufferRef.set(b, file);
   return buffers[file];
 }
@@ -81,14 +75,14 @@ function WriteFile(file, tok) {
 
 function DumpLexer(lex) {
   const { size, pos, start, line, column, lineStart, lineEnd, columnIndex } = lex;
-
   return 'Lexer ' + inspect({ start, pos, size });
 }
+
 function DumpToken(tok) {
   const { length, offset, chars, loc } = tok;
-
   return `★ Token ${inspect({ chars, offset, length, loc }, { depth: 1 })}`;
 }
+
 const What = {
   IMPORT: 0,
   EXPORT: 1
@@ -156,7 +150,6 @@ function AddExport(tokens) {
 }
 
 function AddImport(tokens) {
-  //console.log('tokens:', tokens);
   let range = [+tokens[0].loc, +tokens.last.loc];
   let code = tokens.map(tok => tok.lexeme).join('');
   tokens = tokens.filter(tok => tok.type != 'whitespace');
@@ -190,7 +183,6 @@ function AddImport(tokens) {
       return specifiers;
     }
   }[type]();
-
   return imp;
 }
 
@@ -220,126 +212,103 @@ function main(...args) {
       maxArrayLength: Infinity,
       compact: 1,
       stringBreakNewline: false,
-      hideKeys: [Symbol.toStringTag /*, 'code'*/]
+      hideKeys: [Symbol.toStringTag]
     }
   });
   console.log('args', args);
-
   let optind = 0;
   let code = 'c';
   let debug,
     files = [];
-
   let params = getOpt(
     {
       output: [true, null, 'o'],
       recursive: [false, null, 'r'],
+      filter: [true, null, 'i'],
       debug: [false, () => (debug = (debug | 0) + 1), 'x'],
       '@': 'file'
     },
     args
   );
-  /* while(args[optind]) {
-    if(/code/.test(args[optind])) code = args[++optind];
-    else if(/(debug|^-x$)/.test(args[optind])) debug = true;
-    else files.push(args[optind]);
-
-    optind++;
-  }*/
   files = params['@'];
   console.log('files', files);
-
   const RelativePath = file => path.join(path.dirname(process.argv[1]), '..', file);
 
   if(!files.length) files.push(RelativePath('lib/util.js'));
-
   for(let file of files) ProcessFile(file);
 
   function ProcessFile(file) {
     console.log(`Loading '${file}'...`);
     const log = (...args) => console.log(`${file}:`, ...args);
-
     let str = file ? BufferFile(file) : code[1];
-    //str = '  return new Map(ret.map(([name, description]) => [name, { url: `https://github.com/${user}/${name}`, description }]));';
+
     let len = str.length;
     let type = path.extname(file).substring(1);
     log('data:', escape(str.slice(0, 100)));
-
     let lex = {
       js: new ECMAScriptLexer(str, file),
       c: new CLexer(str, CLexer.LONGEST, file),
       bnf: new BNFLexer(str, file),
       csv: new CSVLexer(str, file)
     };
-
     lex.h = lex.c;
     lex.g4 = lex.bnf;
     lex.ebnf = lex.bnf;
     lex.l = lex.bnf;
     lex.y = lex.bnf;
     lex.json = lex.js;
-
     const lexer = lex[type];
 
     console.log('lexer', lexer);
-    //console.log('lexer.rules', lexer.rules);
-
-    /*console.log(
-      'keys(BNFLexer.prototype,10,0).filter(n => typeof BNFLexer.prototype[n] == "function")',
-      keys(Lexer.prototype, 10, 0).filter(TryCatch(n => typeof Lexer.prototype[n] == 'function').catch(() => {}))
-    );*/
     T = lexer.tokens.reduce((acc, name, id) => ({ ...acc, [name]: id }), {});
 
     log('lexer:', lexer.constructor.name);
-    //log('lexer:', lexer[Symbol.toStringTag]);
+    //log('lexer.tokens:', lexer.tokens);
     log('code:', code);
 
     lexer.handler = lex => {
       const { loc, mode, pos, start, byteLength, state } = lex;
-      //log(`${this.currentLine()}`);
-      //log(`handler loc=${loc} mode=${IntToBinary(mode)} state=${lex.topState()}`, { pos, start, byteLength }, `\n${lex.currentLine()}` );
       log(' '.repeat(loc.column - 1) + '^');
     };
     let tokenList = [],
       declarations = [];
     const colSizes = [12, 8, 4, 20, 32, 10, 0];
 
-    const printTok = debug
-      ? (tok, prefix) => {
-          //if(tok.type=='whitespace')return;
-          const range = tok.charRange;
-          const [start, end] = tok.charRange;
-          //log('printTok', {start,end});
-          let s = toString(str).slice(start, end);
+    let filter = ((params.filter ?? '') + '')
+      .split(/[,\s]+/g)
+      .filter(i => i in lexer.rules || !isNaN(+i))
+      .map(i => (i in lexer.rules ? lexer.rules[i] : i));
+    const printTok =
+      debug || (filter && filter.length > 0)
+        ? (tok, prefix) => {
+            const range = tok.charRange;
+            const [start, end] = tok.charRange;
+            let s = toString(str).slice(start, end);
 
-          const cols = [
-            prefix,
-            `tok[${tok.byteLength}]`,
-            tok.id,
-            tok.type,
-            tok.lexeme,
-            tok.lexeme.length,
-            tok.loc,
-            ` '${s}'`
-          ];
-          std.puts(
-            cols.reduce((acc, col, i) => acc + (col + '').replaceAll('\n', '\\n').padEnd(colSizes[i]), '') + '\n'
-          );
-        }
-      : () => {};
-
+            const cols = [
+              prefix,
+              `tok[${tok.byteLength}]`,
+              tok.id,
+              tok.type,
+              tok.lexeme,
+              tok.lexeme.length,
+              tok.loc,
+              ` '${s}'`
+            ];
+            std.out.puts(
+              cols.reduce((acc, col, i) => acc + (col + '').replaceAll('\n', '\\n').padEnd(colSizes[i]), '') + '\n'
+            );
+          }
+        : () => {};
     let tok,
       i = 0;
-
     log('now', Date.now());
 
     log(lexer.ruleNames.length, 'rules', lexer.ruleNames.unique().length, 'unique rules');
-
     log('lexer.mask', IntToBinary(lexer.mask));
     log('lexer.skip', lexer.skip);
     log('lexer.skip', IntToBinary(lexer.skip));
     log('lexer.states', lexer.states);
-
     log(`new Location(10, 3, 28, 'file.txt')`, new Location(10, 3, 28, 'file.txt'));
     let mask = IntToBinary(lexer.mask);
     let state = lexer.topState();
@@ -363,7 +332,6 @@ function main(...args) {
           case ')': {
             if(stack.last != table[tok.lexeme])
               throw new Error(`top '${stack.last}' != '${tok.lexeme}' [ ${stack.map(s => `'${s}'`).join(', ')} ]`);
-
             stack.pop();
             break;
           }
@@ -374,57 +342,46 @@ function main(...args) {
         reset() {
           stack.clear();
         },
-        /* prettier-ignore */ get depth() {
-        return stack.length;
-      }
+        get depth() {
+          return stack.length;
+        }
       });
-
       return self;
     };
     let balancers = [balancer()];
-
     let imports = [],
       exports = [],
       impexp,
       cond,
       imp = [],
       count = 0;
-
-    /* console.log('lexer.rules', Object.fromEntries(lexer.ruleNames.map(n => [n, new RegExp(lexer.getRule(n)[1], 's')])));
-    console.log('lexer.states', lexer.states);
-    console.log('lexer.tokens', lexer.tokens);*/
-
     let showToken = tok => {
-      // if(tok.type=='whitespace')return;
       if(
         (lexer.constructor != ECMAScriptLexer && tok.type != 'whitespace') ||
         /^((im|ex)port|from|as)$/.test(tok.lexeme)
       ) {
-        // console.log('token', { lexeme: tok.lexeme, id: tok.id, loc: tok.loc + '' });
-        let a = [/*(file + ':' + tok.loc).padEnd(file.length+10),*/ tok.type.padEnd(20, ' '), escape(tok.lexeme)];
+        let a = [tok.type.padEnd(20, ' '), escape(tok.lexeme)];
         std.puts(a.join('') + '\n');
       }
     };
-
     let it = lexer[Symbol.iterator]();
-    console.log('it', it);
 
+    console.log('it', it);
+    console.log('params.filter', params.filter);
+    console.log('filter', filter);
     for(;;) {
       let { stateDepth } = lexer;
       let nextTok = it.next();
       let { done, value } = nextTok;
       if(done) break;
-      //console.log('value', value);
       count++;
       let newState = lexer.topState();
-      //showToken(tok);
       if(newState != state) {
         if(state == 'TEMPLATE' && lexer.stateDepth > stateDepth) balancers.push(balancer());
         if(newState == 'TEMPLATE' && lexer.stateDepth < stateDepth) balancers.pop();
       }
       let n = balancers.last.depth;
       if(!(tok = lexer.token)) break;
-
       if(n == 0 && tok.lexeme == '}' && lexer.stateDepth > 0) {
         lexer.popState();
       } else {
@@ -433,7 +390,7 @@ function main(...args) {
         if(['import', 'export'].indexOf(tok.lexeme) >= 0) {
           impexp = What[tok.lexeme.toUpperCase()];
           let prev = tokens[tokens.length - 1];
-          /* if(tokens.length == 0 || prev.lexeme.endsWith('\n')) */ {
+          {
             cond = true;
             imp = [];
           }
@@ -445,63 +402,45 @@ function main(...args) {
             if(imp.some(i => i.lexeme == 'from')) {
               if(impexp == What.IMPORT) imports.push(AddImport(imp));
             }
-
             if(impexp == What.EXPORT) exports.push(AddExport(imp));
           }
         }
-        if(tok.type != 'whitespace') printTok(tok, newState);
+        if(filter && filter.length > 0 ? filter.contains(tok.id) : tok.type != 'whitespace') printTok(tok, newState);
         tokens.push(tok);
       }
       state = newState;
     }
-
     const exportTokens = tokens.reduce((acc, tok, i) => (tok.lexeme == 'export' ? acc.concat([i]) : acc), []);
     log('Export tokens', exportTokens);
-
     const exportNames = exportTokens.map(index => ExportName(tokens.slice(index)));
     log('Export names', exportNames);
-
     log('ES6 imports', imports.map(PrintES6Import));
     log('CJS imports', imports.map(PrintCJSImport));
-
     std.puts(`import { ${exportNames.join(', ')} } from '${file}'\n`);
 
     modules[file] = { imports, exports };
-
-    //  log('exports', exports);
     let fileImports = imports.filter(imp => /\.js$/i.test(imp.file));
     let splitPoints = unique(fileImports.reduce((acc, imp) => [...acc, ...imp.range], []));
     buffers[file] = [...split(BufferFile(file), ...splitPoints)].map(b => b ?? toString(b, 0, b.byteLength));
     log(`splitPoints`, splitPoints);
-    //log(`buffers[${file}]`, buffers[file]);
-    /*log(`buffers[${file}] len`, BufferLengths(file));
-    log(`buffers[${file}] ofs`, BufferOffsets(file));
-    log(`buffers[${file}] rng`, BufferRanges(file));*/
     log(
       'fileImports',
       fileImports.map(imp => imp.file)
     );
-
     let dir = path.dirname(file);
 
     if(params.recursive)
       fileImports.forEach(imp => {
         let p = path.collapse(path.join(dir, imp.file));
         log('p', p);
-
         AddUnique(files, p);
-        //    let f = BufferFile(p);
       });
-
     let end = Date.now();
 
     log(`took ${end - start}ms (${count} tokens)`);
     log('lexer', lexer);
-    //log('tokens', tokens);
-
     std.gc();
   }
-  // console.log('buffers', buffers);
   console.log('files', files);
 }
 
