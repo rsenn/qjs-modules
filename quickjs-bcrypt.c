e0f187827187c024caf0cd529fd0709652c69a27
Roman Senn <roman.l.senn@gmail.com>
2025-05-25 17:36:10 +0200

    ...

---
+++ quickjs-bcrypt.c
@@ -0,0 +1,124 @@
+#include <quickjs.h>
+#include "libbcrypt/bcrypt.h"
+#include "buffer-utils.h"
+
+enum {
+  BCRYPT_GENSALT,
+  BCRYPT_CHECKPW,
+  BCRYPT_HASHPW,
+};
+
+static JSValue
+js_bcrypt_function(JSContext* ctx, JSValueConst this_val, int argc, JSValueConst argv[], int magic) {
+  JSValue ret;
+  InputBuffer input = js_input_chars(ctx, argv[0]);
+
+  switch(magic) {
+    case BCRYPT_GENSALT: {
+      uint32_t wf = 12;
+
+      JS_ToUint32(ctx, &wf, argv[0]);
+
+      if(argc > 1) {
+        InputBuffer salt = js_input_buffer(ctx, argv[1]);
+
+        if(salt.size < BCRYPT_HASHSIZE)
+          return JS_ThrowInternalError(ctx,
+                                       "supplied buffer size (%lu) < %d",
+                                       (unsigned long)salt.size,
+                                       BCRYPT_HASHSIZE);
+
+        ret = JS_NewInt32(ctx, bcrypt_gensalt(wf, (void*)salt.data));
+
+        input_buffer_free(&salt, ctx);
+      } else {
+        char s[BCRYPT_HASHSIZE + 1];
+
+        memset(s, 0, sizeof(s));
+
+        ret = bcrypt_gensalt(wf, s) ? JS_NULL : JS_NewStringLen(ctx, s, strlen(s));
+      }
+
+      break;
+    }
+    case BCRYPT_HASHPW: {
+      const char* pw = JS_ToCString(ctx, argv[0]);
+      InputBuffer salt = js_input_chars(ctx, argv[1]);
+      InputBuffer buf = js_input_buffer(ctx, argv[2]);
+      char s[BCRYPT_HASHSIZE];
+
+      memset(s, 0, sizeof(s));
+
+      if(!salt.size)
+        bcrypt_gensalt(12, s);
+      else
+        memcpy(s, salt.data, salt.size);
+
+      input_buffer_free(&salt, ctx);
+
+      if(buf.size < BCRYPT_HASHSIZE)
+        return JS_ThrowInternalError(ctx, "supplied buffer size (%lu) < %d", (unsigned long)buf.size, BCRYPT_HASHSIZE);
+
+      ret = JS_NewInt32(ctx, bcrypt_hashpw(pw, s, (char*)buf.data));
+
+      input_buffer_free(&buf, ctx);
+      JS_FreeCString(ctx, pw);
+      break;
+    }
+
+    case BCRYPT_CHECKPW: {
+      const char* pw = JS_ToCString(ctx, argv[0]);
+      InputBuffer buf = js_input_chars(ctx, argv[1]);
+      char x[BCRYPT_HASHSIZE];
+
+      if(!buf.size)
+        return JS_ThrowInternalError(ctx, "supplied buffer size 0");
+
+      memset(x, 0, sizeof(x));
+      memcpy(x, buf.data, buf.size);
+
+      input_buffer_free(&buf, ctx);
+
+      ret = JS_NewInt32(ctx, bcrypt_checkpw(pw, x));
+
+      JS_FreeCString(ctx, pw);
+      break;
+    }
+  }
+
+  return ret;
+}
+
+static const JSCFunctionListEntry js_bcrypt_functions[] = {
+    JS_CFUNC_MAGIC_DEF("gensalt", 0, js_bcrypt_function, BCRYPT_GENSALT),
+    JS_CFUNC_MAGIC_DEF("hashpw", 1, js_bcrypt_function, BCRYPT_HASHPW),
+    JS_CFUNC_MAGIC_DEF("checkpw", 2, js_bcrypt_function, BCRYPT_CHECKPW),
+};
+
+static int
+js_bcrypt_init(JSContext* ctx, JSModuleDef* m) {
+  if(m)
+    JS_SetModuleExportList(ctx, m, js_bcrypt_functions, countof(js_bcrypt_functions));
+
+  return 0;
+}
+
+#if defined(JS_SHARED_LIBRARY) && defined(JS_BCRYPT_MODULE)
+#define JS_INIT_MODULE js_init_module
+#else
+#define JS_INIT_MODULE js_init_module_bcrypt
+#endif
+
+VISIBLE JSModuleDef*
+JS_INIT_MODULE(JSContext* ctx, const char* module_name) {
+  JSModuleDef* m;
+
+  if((m = JS_NewCModule(ctx, module_name, js_bcrypt_init)))
+    JS_AddModuleExportList(ctx, m, js_bcrypt_functions, countof(js_bcrypt_functions));
+
+  return m;
+}
+
+/**
+ * @}
+ */
--- src/child-process.c
+++ src/child-process.c
@@ -39,32 +39,32 @@
 
 /* If WIFEXITED(STATUS), the low-order 8 bits of the status.  */
 #ifndef WEXITSTATUS
-#define WEXITSTATUS(status)   (((status) & 0xff00) >> 8)
+#define WEXITSTATUS(status) (((status)&0xff00) >> 8)
 #endif
 
 /* If WIFSIGNALED(STATUS), the terminating signal.  */
 #ifndef WTERMSIG
-#define WTERMSIG(status)      ((status) & 0x7f)
+#define WTERMSIG(status) ((status)&0x7f)
 #endif
 
 /* If WIFSTOPPED(STATUS), the signal that stopped the child.  */
 #ifndef WSTOPSIG
-#define WSTOPSIG(status)      WEXITSTATUS(status)
+#define WSTOPSIG(status) WEXITSTATUS(status)
 #endif
 
 /* Nonzero if STATUS indicates normal termination.  */
 #ifndef WIFEXITED
-#define WIFEXITED(status)       (((status) & 0x7f) == 0)
+#define WIFEXITED(status) (((status)&0x7f) == 0)
 #endif
 
 /* Nonzero if STATUS indicates the child is stopped.  */
 #ifndef WIFSTOPPED
-#define WIFSTOPPED(status)      (((status) & 0xff) == 0x7f)
+#define WIFSTOPPED(status) (((status)&0xff) == 0x7f)
 #endif
 
 /* Nonzero if STATUS indicates termination by a signal.  */
 #ifndef WIFSIGNALED
-#define WIFSIGNALED(status)     (!WIFSTOPPED(status) && !WIFEXITED(status))
+#define WIFSIGNALED(status) (!WIFSTOPPED(status) && !WIFEXITED(status))
 #endif
 
 /**
