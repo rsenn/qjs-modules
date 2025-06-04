fef561c7f4b630f8a3e1f4a196bb68a290d16ea6
Roman Senn <roman.l.senn@gmail.com>
2025-06-04 13:38:27 +0200

    ...

--- src/bitset.c
+++ src/bitset.c
@@ -19,33 +19,30 @@ bitset_resize(BitSet* bs, size_t bits) {
   return true;
 }
 
-static uint8_t*
-bitset_byte(BitSet* bs, size_t bit) {
-  return &bs->ptr[bit >> 3];
-}
+#define bitset_byte(bs, bit) ((bs)->ptr[(bit) >> 3])
 
 static inline size_t
-bitset_index(BitSet* bs,int32_t idx) {
+bitset_index(BitSet* bs, int32_t idx) {
   idx = WRAP_NUM(idx, (signed)bs->len);
 
   assert(idx >= 0);
   assert(idx < bs->len);
 
-return idx;
+  return idx;
 }
 
 bool
 bitset_at(BitSet* bs, int32_t idx) {
-  size_t bit=bitset_index(idx);
-    uint8_t* b = bitset_byte(bs, bit);
+  size_t bit = bitset_index(idx);
+  uint8_t b = bitset_byte(bs, bit);
 
-  return ((*b) >> (bit & 7)) & 1;
+  return (b >> (bit & 7)) & 1;
 }
 
 void
-bitset_assign(BitSet* bs,  int32_t idx, bool value) {
-   size_t bit=bitset_index(idx);
- uint8_t* b = bitset_byte(bs, bit);
+bitset_assign(BitSet* bs, int32_t idx, bool value) {
+  size_t bit = bitset_index(idx);
+  uint8_t* b = &bitset_byte(bs, bit);
   uint8_t mask = 1 << (bit & 7);
 
   (*b) = value ? (*b) | mask : (*b) & (~mask);
@@ -53,8 +50,8 @@ bitset_assign(BitSet* bs,  int32_t idx, bool value) {
 
 bool
 bitset_toggle(BitSet* bs, int32_t idx) {
-    size_t bit=bitset_index(idx);
- uint8_t* b = bitset_byte(bs, bit);
+  size_t bit = bitset_index(idx);
+  uint8_t* b = &bitset_byte(bs, bit);
   uint8_t mask = 1 << (bit & 7);
 
   (*b) ^= mask;
